bd3b7af249bcbf1295acbea6dda71c4f19873f80
LorenzoDellera <100216966+LorenzoDellera@users.noreply.github.com>
2022-05-14 16:14:12 +0200

    gui update

--- src/main/java/it/polimi/ingsw/view/gui/SceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/SceneController.java
@@ -2,18 +2,28 @@ package it.polimi.ingsw.view.gui;
 
 import it.polimi.ingsw.observer.ClientObservable;
 import it.polimi.ingsw.view.gui.scene.BasicSceneController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SceneController extends ClientObservable {
+    private static Stage stage;
     private static Scene actualScene;
     private static BasicSceneController actualController;
+
+    public SceneController(Stage primaryStage, Scene actualScene, BasicSceneController actualController) {
+        this.stage = primaryStage;
+        this.actualScene = actualScene;
+        this.actualController = actualController;
+    }
 
     /**
      * Returns the actual scene
      *
      * @return actual scene
      */
-    public static Scene getActualScene() {
+    public Scene getActualScene() {
         return actualScene;
     }
 
@@ -22,8 +32,17 @@ public class SceneController extends ClientObservable {
      *
      * @return actual controller
      */
-    public static BasicSceneController getActualController() {
+    public BasicSceneController getActualController() {
         return actualController;
+    }
+
+    public static void changeScene(Scene newScene) {
+        stage.setScene(newScene);
+        stage.setWidth(1280d);
+        stage.setHeight(720d);
+        stage.setResizable(true);
+        stage.setTitle("Eriantys - by Giovanni De Lucia, Lorenzo Battiston, Lorenzo Dell'Era");
+        stage.show();
     }
 
 }
