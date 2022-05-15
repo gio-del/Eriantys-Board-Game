651cb781a186184d291b827463bb6320118dd9ff
LorenzoDellera <100216966+LorenzoDellera@users.noreply.github.com>
2022-05-15 17:40:32 +0200

    gui update, scene switching implementation

--- src/main/java/it/polimi/ingsw/view/gui/SceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/SceneController.java
@@ -2,27 +2,38 @@ package it.polimi.ingsw.view.gui;
 
 import it.polimi.ingsw.observer.ClientObservable;
 import it.polimi.ingsw.view.gui.scene.BasicSceneController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class SceneController extends ClientObservable {
-    private static Stage stage;
     private static Scene actualScene;
     private static BasicSceneController actualController;
 
-    public SceneController(Stage primaryStage, Scene actualScene, BasicSceneController actualController) {
-        stage = primaryStage;
+    public SceneController(Scene actualScene, BasicSceneController actualController) {
         SceneController.actualScene = actualScene;
         SceneController.actualController = actualController;
     }
 
-    public static void changeScene(Scene newScene) {
-        stage.setScene(newScene);
-        stage.setWidth(1280d);
-        stage.setHeight(720d);
-        stage.setResizable(true);
-        stage.setTitle("Eriantys - by Giovanni De Lucia, Lorenzo Battiston, Lorenzo Dell'Era");
-        stage.show();
+    /**
+     * This method change the actual scene
+     *
+     * @param actual is the ctual scene
+     * @param newFXML is the fxml file reference to the new scene
+     */
+    public static void changeScene(Scene actual, String newFXML) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(SceneController.class.getResource(newFXML));
+        } catch (IOException e) {
+            //avvisare client prima
+            System.exit(1);
+        }
+        Stage stage = (Stage) actual.getWindow();
+        stage.setScene(new Scene(root,1220d,700d));
     }
 
     /**
